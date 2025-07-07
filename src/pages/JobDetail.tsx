17f26284ee4194c59bef83f503636c57b30c8df1
Vighnesh Tirakapadi <vighneshvt2000.com>
2025-07-07 15:42:24 +0530

    corrected some ui

--- src/pages/JobDetail.tsx
+++ src/pages/JobDetail.tsx
@@ -11,12 +11,12 @@ const JobDetail = (): React.JSX.Element => {
   const currentJob = jobs?.find((job) => job.id === Number(jobId));
 
   return (
-    <div className="max-w-3xl mx-auto my-6 rounded-2xl overflow-hidden shadow-lg border border-gray-200">
+    <div className="max-w-3xl my-6 rounded-2xl overflow-hidden shadow-lg border border-gray-200 md:mt-40 mx-6">
       <div className="bg-gradient-to-r from-purple-700 to-fuchsia-600 p-6 text-white flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
         <div>
           <div className="flex justify-between">
             <h2 className="text-2xl font-semibold">{currentJob?.title}</h2>
-            <span className="bg-green-200 text-green-800 px-3 py-1 rounded-full font-medium text-sm flex items-center">
+            <span className="bg-green-200 text-green-800 px-3 rounded-full font-medium text-sm flex items-center text-nowrap h-10">
               {currentJob?.ai_match_score}% Match
             </span>
           </div>
@@ -71,10 +71,10 @@ const JobDetail = (): React.JSX.Element => {
               </div>
             </div>
           </div>
-          <button className="flex items-center justify-center gap-2 w-full bg-blue-700 hover:bg-blue-800 text-white py-3 rounded-lg transition-all my-2">
+          <button className="flex items-center justify-center gap-2 w-full bg-blue-700 hover:bg-blue-800 text-white py-3 rounded-lg transition-all my-2 cursor-pointer">
             <FaPlus /> Apply Now
           </button>
-          <button className="w-full bg-gray-100 hover:bg-gray-200 text-gray-800 py-2 rounded-lg transition-all my-2">
+          <button className="w-full bg-gray-100 hover:bg-gray-200 text-gray-800 py-3 rounded-lg transition-all my-2 cursor-pointer">
             Back to Jobs
           </button>
         </div>
